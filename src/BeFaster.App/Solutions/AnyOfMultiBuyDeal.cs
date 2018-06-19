b3bac07c6483f801be164b9ab55a641a61fa409d
sbx_user1059 <sbx_user1059@ip-10-12-81-71.eu-west-2.compute.internal>
2018-06-19 21:42:13 +0000

    Tue Jun 19 21:42:13 UTC 2018

--- src/BeFaster.App/Solutions/AnyOfMultiBuyDeal.cs
+++ src/BeFaster.App/Solutions/AnyOfMultiBuyDeal.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Linq;
 
 namespace BeFaster.App.Solutions
 {
     public class AnyOfMultiBuyDeal : IMultiDeal
     {
+        public char[] Products { get; }
+        public int NumberRequired { get; }
+        public int Price { get; }
+
         public AnyOfMultiBuyDeal(int i, int price, params char[] c)
         {
-
+            Products = c.OrderByDescending(x => MultiDealEngine.Catalog[x].Price).ToArray();
+            NumberRequired = i;
+            Price = price;
         }
 
         public int CalculatePossibleSavings(char[] characters)
