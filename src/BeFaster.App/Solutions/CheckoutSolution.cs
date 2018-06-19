5c60d43a5f9f15fba027d3ba2a6adc9bf55ee8e1
sbx_user1059 <sbx_user1059@ip-10-12-81-71.eu-west-2.compute.internal>
2018-06-19 16:52:41 +0000

    Tue Jun 19 16:52:41 UTC 2018

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -1,12 +1,40 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions
 {
+    public static class ProductPricing
+    {
+        
+    }
+
+    public class Product
+    {
+        public int Price { get; }
+        public Func<int, int> MultiBuyFunc { get; }
+
+        public Product(int price, Func<int, int> multiBuyFunc = null)
+        {
+            Price = price;
+
+            if(multiBuyFunc == null)
+                MultiBuyFunc = x => price*x;
+            else
+                MultiBuyFunc = multiBuyFunc;
+        }
+    }
+
     public static class CheckoutSolution
     {
+        
+
+
         public static int Checkout(string skus)
         {
-            throw new SolutionNotImplementedException();
+            var delimiters = new[] {","};
+
+            var productsPurchased = skus.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
         }
     }
 }
