17e036b20a61ccacd66849a3ab7aaaa3e4b01aa6
sbx_user1059 <sbx_user1059@ip-10-12-81-71.eu-west-2.compute.internal>
2018-06-19 19:58:12 +0000

    Tue Jun 19 19:58:12 UTC 2018

--- src/BeFaster.App/Solutions/MultiDealEngine.cs
+++ src/BeFaster.App/Solutions/MultiDealEngine.cs
@@ -87,7 +87,7 @@ namespace BeFaster.App.Solutions
                 }
             } while (j < baseCombination.Count);
 
-            return resultSet.Distinct(new IntArrayComparison()).ToList();
+            return resultSet.Distinct(x => new IntArrayComparison()).ToList();
         }
 
         public static void Swap(int l, int r, int[] array)
@@ -124,7 +124,13 @@ namespace BeFaster.App.Solutions
     {
         public bool Equals(List<int> x, List<int> y)
         {
-            
+            for (var i = 0; i < x.Count; i++)
+            {
+                if (x[i] != y[i])
+                    return false;
+            }
+
+            return true;
         }
 
         public int GetHashCode(List<int> obj)
