0c2b54f606d34dddaf7f7f461d42e4d668f5a8a8
sbx_user1059 <sbx_user1059@ip-10-12-81-71.eu-west-2.compute.internal>
2018-06-19 17:37:12 +0000

    Tue Jun 19 17:37:12 UTC 2018

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -5,35 +5,59 @@ using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions
 {
+    public class MultiDeal
+    {
+        public Func<bool, IEnumerable<char>> FindDealFunc;
+
+        public IEnumerable<MultiDeal> Conflicts;
+
+        public Func<IEnumerable<char>, Tuple<IEnumerable<char>, int>> Apply;
+    }
+
+    public static class MultiDealEngine
+    {
+        List<MultiDeal> deals = new List<MultiDeal>();
+
+        static MultiDealEngine()
+        {
+            
+        }
+    }
+
     public static class CheckoutSolution
     {
         private static Dictionary<char, Product> Catalog;
+        private static Dictionary<char, Func<string, IEnumerable<string>, int>> dealMapFunctions;
 
         static CheckoutSolution()
         {
             Catalog = new Dictionary<char, Product>();
-
-            Catalog.Add('A', new Product(50, new MultiBuy(3, 130)));
-            Catalog.Add('B', new Product(30, new MultiBuy(2, 45)));
+            Catalog.Add('A', new Product(50));
+            Catalog.Add('B', new Product(30));
             Catalog.Add('C', new Product(20));
             Catalog.Add('D', new Product(15));
+            Catalog.Add('E', new Product(40));
         }
+
+        
 
         public static int Checkout(string skus)
         {
             try
             {
-
                 var productsPurchased = skus.ToCharArray();
 
                 var distinctProducts = productsPurchased.Distinct();
 
                 var total = 0;
 
+
                 foreach (var distinctProduct in distinctProducts)
                 {
                     var numOfProduct = productsPurchased.Count(x => x == distinctProduct);
-                    total += Catalog[distinctProduct].PriceQuantity(numOfProduct);
+
+
+
                 }
 
                 return total;
