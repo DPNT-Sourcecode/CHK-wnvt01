de2a7df763c983a7de810650afbcf5448df5f1dc
sbx_user1059 <sbx_user1059@ip-10-12-81-71.eu-west-2.compute.internal>
2018-06-19 18:31:12 +0000

    Tue Jun 19 18:31:12 UTC 2018

--- src/BeFaster.App/Solutions/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CheckoutSolution.cs
@@ -5,64 +5,16 @@ using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions
 {
-    public static class MultiDealEngine
-    {
-        static List<MultiDeal> deals = new List<MultiDeal>();
-
-        static MultiDealEngine()
-        {
-            deals.Add(new SimpleMultiBuyDeal(3, 130, 'A'));
-            deals.Add(new SimpleMultiBuyDeal(5, 200, 'A'));
-            deals.Add(new SimpleMultiBuyDeal(2, 45, 'B'));
-            deals.Add(new MultiBuyRemovalDeal(2,'E', 1,'B'));
-        }
-
-        public static List<Tuple<MultiDeal, int>> FindDeals(char[] skus)
-        {
-            var foundDeals = new List<Tuple<MultiDeal, int>>();
-
-            foreach (var multiDeal in deals)
-            {
-                var possibleApplications = multiDeal.CountNumberOfTimesCanBeApplied(skus);
-
-                if(possibleApplications > 0)
-                    foundDeals.Add(new Tuple<MultiDeal, int>(multiDeal, possibleApplications));
-            }
-
-            return foundDeals;
-        }
-
-        public static List<List<MultiDeal>> ComputeCombinations(List<Tuple<MultiDeal, int>> possibleDeals)
-        {
-            return new List<List<MultiDeal>>();
-        }
-    }
-
     public static class CheckoutSolution
     {
-        private static Dictionary<char, Product> Catalog;
-
-        static CheckoutSolution()
-        {
-            Catalog = new Dictionary<char, Product>();
-            Catalog.Add('A', new Product(50));
-            Catalog.Add('B', new Product(30));
-            Catalog.Add('C', new Product(20));
-            Catalog.Add('D', new Product(15));
-            Catalog.Add('E', new Product(40));
-        }
-
-        
-
         public static int Checkout(string skus)
         {
             try
             {
                 var productsPurchased = skus.ToCharArray().OrderBy(x => x).ToArray();
-
-                var total = 0;
-
-                return total;
+                var foundPossibleDeals = MultiDealEngine.FindDeals(productsPurchased);
+                var combinations = MultiDealEngine.ComputeCombinations(foundPossibleDeals);
+                return combinations.Select(x => MultiDealEngine.Apply(productsPurchased)).AsParallel().Min();
             }
             catch (Exception)
             {
---
+++ src/BeFaster.App/Solutions/MultiDealEngine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BeFaster.App.Solutions
+{
+    public static class MultiDealEngine
+    {
+        static List<MultiDeal> deals = new List<MultiDeal>();
+        private static Dictionary<char, Product> Catalog;
+
+        static MultiDealEngine()
+        {
+            deals.Add(new SimpleMultiBuyDeal(3, 130, 'A'));
+            deals.Add(new SimpleMultiBuyDeal(5, 200, 'A'));
+            deals.Add(new SimpleMultiBuyDeal(2, 45, 'B'));
+            deals.Add(new MultiBuyRemovalDeal(2,'E', 1,'B'));
+            Catalog = new Dictionary<char, Product>();
+            Catalog.Add('A', new Product(50));
+            Catalog.Add('B', new Product(30));
+            Catalog.Add('C', new Product(20));
+            Catalog.Add('D', new Product(15));
+            Catalog.Add('E', new Product(40));
+        }
+
+        public static List<Tuple<MultiDeal, int>> FindDeals(char[] skus)
+        {
+            var foundDeals = new List<Tuple<MultiDeal, int>>();
+
+            foreach (var multiDeal in deals)
+            {
+                var possibleApplications = multiDeal.CountNumberOfTimesCanBeApplied(skus);
+
+                if(possibleApplications > 0)
+                    foundDeals.Add(new Tuple<MultiDeal, int>(multiDeal, possibleApplications));
+            }
+
+            return foundDeals;
+        }
+
+        public static List<List<MultiDeal>> ComputeCombinations(List<Tuple<MultiDeal, int>> possibleDeals)
+        {
+            return new List<List<MultiDeal>>();
+        }
+
+        public static int Apply(char[] productsPurchased)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
